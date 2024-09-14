a749ae25567f12b4ae6efe51f45f425b1da9ddb2
Animesh Kumar <120721959+Animesh-X@users.noreply.github.com>
2024-09-15 00:04:27 +0530

    "Added owner variable, lockup period, and early withdrawal penalty to Staking contract; modified constructor, rewardPerToken, earned, stake, withdrawStakedTokens, and getReward functions; added new event PenaltyTransferred."

--- staking/contracts/Staking.sol
+++ staking/contracts/Staking.sol
@@ -5,75 +5,100 @@ pragma solidity >=0.8.2 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Staking is ReentrancyGuard{
-  using SafeMath for uint256;
-  IERC20 public s_stakingToken;
-  IERC20 public s_rewardToken;
+contract Staking is ReentrancyGuard {
+    using SafeMath for uint256;
+    IERC20 public s_stakingToken;
+    IERC20 public s_rewardToken;
 
-  uint public constant REWARD_RATE=1e18;
-  uint private totalStakedTokens;
-  uint public rewardPerTokenStored;
-  uint public lastUpdateTime;
+    uint public constant REWARD_RATE = 1e18;
+    uint private totalStakedTokens;
+    uint public rewardPerTokenStored;
+    uint public lastUpdateTime;
+    address public owner;
 
-  mapping(address=>uint) public stakedBalance;
-  mapping(address=>uint) public rewards;
-  mapping(address=>uint) public userRewardPerTokenPaid;
+    uint public constant LOCKUP_PERIOD = 2 days;
+    uint public constant EARLY_WITHDRAWAL_PENALTY = 10; // 10% penalty
 
-  event Staked(address indexed user, uint256 indexed amount);
-  event Withdrawn(address indexed user, uint256 indexed amount);
-  event RewardsClaimed(address indexed user, uint256 indexed amount);
-  
-  constructor(address stakingToken,address rewardToken){
-    s_stakingToken=IERC20(stakingToken);
-    s_rewardToken=IERC20(rewardToken);
-  }
+    mapping(address => uint) public stakedBalance;
+    mapping(address => uint) public rewards;
+    mapping(address => uint) public userRewardPerTokenPaid;
+    mapping(address => uint) public lastStakeTime;
 
-  function rewardPerToken() public view returns(uint){
-    if(totalStakedTokens==0){
-        return rewardPerTokenStored;
+    event Staked(address indexed user, uint256 indexed amount);
+    event Withdrawn(address indexed user, uint256 indexed amount, uint256 penalty);
+    event RewardsClaimed(address indexed user, uint256 indexed amount);
+    event PenaltyTransferred(address indexed user, uint256 indexed amount);
+    
+    constructor(address stakingToken, address rewardToken) {
+        s_stakingToken = IERC20(stakingToken);
+        s_rewardToken = IERC20(rewardToken);
+        owner = msg.sender;
     }
-    uint totalTime = block.timestamp.sub(lastUpdateTime);
-    uint totalRewards = REWARD_RATE.mul(totalTime); 
-    return rewardPerTokenStored.add(totalRewards.mul(1e18).div(totalStakedTokens));
-  }
 
-  function earned(address account) public view returns(uint){
-    return stakedBalance[account].mul(rewardPerToken().sub(userRewardPerTokenPaid[account])).div(1e18).add(rewards[account]);
-  }
+    function rewardPerToken() public view returns (uint) {
+        if (totalStakedTokens == 0) {
+            return rewardPerTokenStored;
+        }
+        uint totalTime = block.timestamp.sub(lastUpdateTime);
+        uint totalRewards = REWARD_RATE.mul(totalTime); 
+        return rewardPerTokenStored.add(totalRewards.mul(1e18).div(totalStakedTokens));
+    }
 
-  modifier updateReward(address account){
-    rewardPerTokenStored=rewardPerToken();
-    lastUpdateTime=block.timestamp;
-    rewards[account]=earned(account);
-    userRewardPerTokenPaid[account]=rewardPerTokenStored;
-    _;
-  }
+    function earned(address account) public view returns (uint) {
+        return stakedBalance[account].mul(rewardPerToken().sub(userRewardPerTokenPaid[account])).div(1e18).add(rewards[account]);
+    }
 
-  function stake(uint amount) external nonReentrant updateReward(msg.sender){
-    require(amount>0,"Amount must be greater than zero");
-    totalStakedTokens=totalStakedTokens.add(amount);
-    stakedBalance[msg.sender]=stakedBalance[msg.sender].add(amount);
-    emit Staked(msg.sender,amount);
-    bool success = s_stakingToken.transferFrom(msg.sender,address(this),amount);
-    require(success,"Transfer Failed");
-  }
-  function withdrawStakedTokens(uint amount) external nonReentrant updateReward(msg.sender)  {
-    require(amount>0,"Amount must be greater than zero");
-    require(stakedBalance[msg.sender]>=amount,"Staked amount not enough");
-    totalStakedTokens=totalStakedTokens.sub(amount);
-    stakedBalance[msg.sender]=stakedBalance[msg.sender].sub(amount);
-    emit Withdrawn(msg.sender, amount);(msg.sender,amount);
-    bool success = s_stakingToken.transfer(msg.sender,amount);
-    require(success,"Transfer Failed");
-  }
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = block.timestamp;
+        rewards[account] = earned(account);
+        userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        _;
+    }
 
-  function getReward() external nonReentrant updateReward(msg.sender){
-     uint reward = rewards[msg.sender];
-     require(reward>0,"No rewards to claim");
-     rewards[msg.sender]=0;
-     emit RewardsClaimed(msg.sender, reward);
-     bool success = s_rewardToken.transfer(msg.sender,reward);
-     require(success,"Transfer Failed");
-  }
+    function stake(uint amount) external nonReentrant updateReward(msg.sender) {
+        require(amount > 0, "Amount must be greater than zero");
+        totalStakedTokens = totalStakedTokens.add(amount);
+        stakedBalance[msg.sender] = stakedBalance[msg.sender].add(amount);
+        lastStakeTime[msg.sender] = block.timestamp;
+        emit Staked(msg.sender, amount);
+        bool success = s_stakingToken.transferFrom(msg.sender, address(this), amount);
+        require(success, "Transfer Failed");
+    }
+
+    function withdrawStakedTokens(uint amount) external nonReentrant updateReward(msg.sender) {
+        require(amount > 0, "Amount must be greater than zero");
+        require(stakedBalance[msg.sender] >= amount, "Staked amount not enough");
+
+        uint penaltyAmount = 0;
+        if (block.timestamp < lastStakeTime[msg.sender].add(LOCKUP_PERIOD)) {
+            penaltyAmount = amount.mul(EARLY_WITHDRAWAL_PENALTY).div(100);
+        }
+
+        uint amountToWithdraw = amount.sub(penaltyAmount);
+        totalStakedTokens = totalStakedTokens.sub(amount);
+        stakedBalance[msg.sender] = stakedBalance[msg.sender].sub(amount);
+
+        emit Withdrawn(msg.sender, amountToWithdraw, penaltyAmount);
+
+        bool success = s_stakingToken.transfer(msg.sender, amountToWithdraw);
+        require(success, "Transfer to user failed");
+
+        if (penaltyAmount > 0) {
+            success = s_stakingToken.transfer(owner, penaltyAmount);
+            require(success, "Transfer of penalty to owner failed");
+            emit PenaltyTransferred(msg.sender, penaltyAmount);
+        }
+    }
+
+    function getReward() external nonReentrant updateReward(msg.sender) {
+        uint reward = rewards[msg.sender];
+        require(reward > 0, "No rewards to claim");
+        rewards[msg.sender] = 0;
+        emit RewardsClaimed(msg.sender, reward);
+        bool success = s_rewardToken.transfer(msg.sender, reward);
+        require(success, "Transfer Failed");
+    }
 }
