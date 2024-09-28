e8995f31f0235de778c877dd843cb561eb63b39e
Animesh Kumar <120721959+Animesh-X@users.noreply.github.com>
2024-09-28 12:07:17 +0530

    refactor: Remove unnecessary files and code
    Removed vite.config.js, Web3Button.jsx, App.jsx, .gitignore, index.html, main.jsx, constants.js, Dashboard.css, StakeXFI.css, Homepage.css, Homepage.jsx, package.json, Dashboard.jsx, config.js, and eslint.config.js files. Also deleted corresponding import statements and CSS classes.

--- defi-app/defi-app/src/abi/constants.js
+++
@@ -1,9 +0,0 @@
-import StakeToken from "./StakeToken.json";
-import Staking from "./Staking.json";
-
-export const STAKE_TOKEN_ABI = StakeToken
-export const STAKING_ABI = Staking
-
-export const REWARD_TOKEN_ADDRESS = "0xAF77f4CE0ee047F7191f54b8564eC800DE3CeF5d"
-export const STAKING_ADDRESS = "0x07eA0AC41dE236f1EC2EB6eDf70dceF2BFbf61f4"
-export const STAKE_TOKEN_ADDRESS = "0x2E33249F0AAB1a41fe4987580bef3A1B8372eC3A"
--- defi-app/defi-app/src/components/Web3Button.jsx
+++
@@ -1,8 +0,0 @@
-
-const Web3Button = () => {
-  return (
-    <w3m-button />
-  )
-}
-
-export default Web3Button
