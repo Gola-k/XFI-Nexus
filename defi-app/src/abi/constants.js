1f21066793f03d5ed689db67098e47d815f04411
praveen836845 <roy200297@gmail.com>
2024-11-06 14:26:53 +0530

    changes

--- defi-app/src/abi/constants.js
+++ defi-app/src/abi/constants.js
@@ -13,24 +13,24 @@ export const STAKE_TOKEN_ABI = StakeToken
 export const STAKING_ABI = Staking
 
 
-export const REWARD_TOKEN_ADDRESS = "0xEfD55a2A8f1e135Ab80d36ec40fE39DF6222Ad9D"
-export const STAKING_ADDRESS = "0x0873C0ceB962aE6021c2c585080C989e306e38aC"
-export const STAKE_TOKEN_ADDRESS = "0x6e33a65E119833470F03A1b819CD07E221bDB559"
+export const REWARD_TOKEN_ADDRESS = "0xbb2BA5158F87982D83804397e2ae2dcac25B274c"
+export const STAKING_ADDRESS = "0x18E8be2295316Ed29c35f2379A1E7950068eF858"
+export const STAKE_TOKEN_ADDRESS = "0x0c8CeB43ac5fc2E3970994e42fBf2bbd0A598E41"
 
 
 export const MockXFI_ABI = mockXFI ;
-export const MockXFI_Address = '0xc7C7e8E0f8601aa1796Dfd81e1E1376ca2bEe77a';
+export const MockXFI_Address = '0x88759405875239F370Df1fDa49e778A20E2d5b87';
 
 export const MockMPX_ABI = mockMPX;
 
-export const MockMPX_Address = '0x0C9A20DAE6BfACf88a82a10ca852B77f32266FB3'; 
+export const MockMPX_Address = '0x793f23DEF080667a352599449f073B05FDa0BC47'; 
 
-export const AMM_Address = "0xBa65bF7df04B67e5802b1459033F821332bBaeF2"
+export const AMM_Address = "0x16438679D4a38EDD62E065F3C6D75AF00DD37E35"
 export const AMM_ABI = AMM;
 
 // Lending and borrowing functionality
-export const XFI_Token_Address = "0x0F2c7Ff8a42117a8899af371CD639447a9E30b0e";
-export const XFI_Lending_Address = "0xF30b4D7d2DC29c101134a0cDc819Ab871D142589" 
+export const XFI_Token_Address = "0x06EeB8D04F08bfbf7D3d14b496df9947Eb0fff78";
+export const XFI_Lending_Address = "0xe32B7485911490A71f2eB2D6B612f7f89Dc9641A" 
 // export const DAI_Address = "0x2bE7c1ad04AEcd5f0C56f4a41BBC3Ac9ADF249f4 ";
 
 
